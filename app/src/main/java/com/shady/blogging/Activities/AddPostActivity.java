f8e5785ff0a27e7012ebc18149739d13d03d042a
ShadyCoding <shahidmahmood.yar@gmail.com>
2020-04-24 23:37:21 +0500

    Initial commit

---
+++ app/src/main/java/com/shady/blogging/Activities/AddPostActivity.java
@@ -0,0 +1,150 @@
+package com.shady.blogging.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.shady.blogging.R;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AddPostActivity extends AppCompatActivity {
+
+    private ImageButton mPostImage;
+    private EditText mPostTitle;
+    private EditText mPostDesc;
+    private Button mSubmitButton;
+    private StorageReference mStorage;
+    private DatabaseReference mPostDatabase;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private ProgressDialog mProgress;
+    private Uri mImageUri;
+    private static final int GALLERY_CODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_post);
+
+        mProgress = new ProgressDialog(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        mStorage = FirebaseStorage.getInstance().getReference();
+
+
+        mPostDatabase = FirebaseDatabase.getInstance().getReference().child("MBlog");
+
+        mPostImage = findViewById(R.id.imageButton);
+        mPostTitle = findViewById(R.id.postTitleEt);
+        mPostDesc = findViewById(R.id.descriptionEt);
+        mSubmitButton = findViewById(R.id.submitPost);
+
+        mPostImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, GALLERY_CODE);
+            }
+        });
+
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Posting to our database
+                startPosting();
+
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_CODE && resultCode == RESULT_OK) {
+            mImageUri = data.getData();
+            mPostImage.setImageURI(mImageUri);
+
+
+        }
+    }
+
+    private void startPosting() {
+
+        mProgress.setMessage("Posting to blog...");
+        mProgress.show();
+
+        final String titleVal = mPostTitle.getText().toString().trim();
+        final String descVal = mPostDesc.getText().toString().trim();
+
+        if (!TextUtils.isEmpty(titleVal) && !TextUtils.isEmpty(descVal)
+                && mImageUri != null) {
+            //start the uploading...
+            //mImageUri.getLastPathSegment() == /image/myphoto.jpeg"
+
+            final StorageReference filepath = mStorage.child("MBlog_images").
+                    child(mImageUri.getLastPathSegment());
+            filepath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    Task<Uri> urlTask = taskSnapshot.getStorage().getDownloadUrl();
+                    while (!urlTask.isSuccessful());
+                    Uri downloadurl = urlTask.getResult();
+                    //Uri downloadurl = taskSnapshot.getDownloadUrl();
+
+                    DatabaseReference newPost = mPostDatabase.push();
+
+
+                    Map<String, String> dataToSave = new HashMap<>();
+                    dataToSave.put("title", titleVal);
+                    dataToSave.put("desc", descVal);
+                    dataToSave.put("image", downloadurl.toString());
+                    dataToSave.put("timestamp", String.valueOf(java.lang.System.currentTimeMillis()));
+                    dataToSave.put("userid", mUser.getUid());
+                    dataToSave.put("username", mUser.getEmail());
+
+                    newPost.setValue(dataToSave);
+
+
+                    //Old way
+//                    newPost.child("title").setValue(titleVal);
+//                    newPost.child("desc").setValue(descrVal);
+//                    newPost.child("image").setValue(downloadUrl.toString());
+//                    newPost.child("timestamp").setValue(java.lang.System.currentTimeMillis());
+
+                    mProgress.dismiss();
+
+                    startActivity(new Intent(AddPostActivity.this, PostListActivity.class));
+                    finish();
+
+                }
+            });
+
+        }
+    }
+}
